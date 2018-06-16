05fa9085b2f4d570fcb5bafe3c2d4c605a749815
Marcia Elyseu <marcia.pelyseu@gmail.com>
2018-06-16 01:22:27 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,20 +1,43 @@
 function forLoop(array) {
   for(let i=0; i<25; i++){
     if(i === 1){
+<<<<<<< HEAD
       array.push("I am 1 strange loop");
     } else {
         array.push("I am ${i} strange loops");
+=======
+      array[i] = "I am 1 strange loop";
+      // console.log("i is:" + i)
+      console.log("array[i] is: " + array[i]);
+      
+    } else {
+      array[i] = "I am " + i + " strange loops";
+      // console.log("i is:" + i)
+      console.log("array[i] is: " + array[i]);
+>>>>>>> baf147f8c55e0a6caf9e084de230f96f094ceb3e
     }
   }
   return array;
 }
 
+<<<<<<< HEAD
 function whileLoop(n){
   while(n > 0){
     console.log("number is equal to :" + n);
     n--;
   }
   return 'done';
+=======
+var a = [];
+forLoop(a);
+
+function whileLoop(n){
+  while(n >= 0){
+    console.log("number is equal to :" + n);
+    n--;
+  }
+  return console.log('done');
+>>>>>>> baf147f8c55e0a6caf9e084de230f96f094ceb3e
 }
 
 whileLoop(5);
